4b231d996fa24b24ce996d989feefb2dd90736e1
Alec McClure <alec_mcclure@ilcp.uscourts.gov>
2025-08-09 12:34:34 -0500

    Added new contracts to mic_Contracts.py and moved the relevant screenshots to DONE folder

--- Code/mic_Contracts.py
+++ Code/mic_Contracts.py
@@ -9,10 +9,9 @@ mic_Contract_1 = Contract(
     contract_Type=planetary,
     contract_Size=extra_Small,
     from_Location=port_Tressler,
-    deliveries=[
-        (5, "Carbon", greycat_IV),
-        (4, "Carbon", sakura_Gold)
-    ],
+    deliveries=[(5, "Carbon", greycat_IV),
+                (4, "Carbon", sakura_Gold)
+                ],
     max_Container=1,
     contract_Pay=61500)
 
@@ -22,10 +21,9 @@ mic_Contract_2 = Contract(
     contract_Type=planetary,
     contract_Size=extra_Small,
     from_Location=port_Tressler,
-    deliveries=[
-        (4, "Titanium", greycat_IV),
-        (3, "Titanium", new_Babbage)
-    ],
+    deliveries=[(4, "Titanium", greycat_IV),
+                (3, "Titanium", new_Babbage)
+                ],
     max_Container=1,
     contract_Pay=61500)
 
@@ -35,10 +33,9 @@ mic_Contract_3 = Contract(
     contract_Type=planetary,
     contract_Size=extra_Small,
     from_Location=port_Tressler,
-    deliveries=[
-        (5, "Quartz", greycat_IV),
-        (4, "Quartz", sakura_Gold)
-    ],
+    deliveries=[(5, "Quartz", greycat_IV),
+                (4, "Quartz", sakura_Gold)
+                ],
     max_Container=1,
     contract_Pay=61500)
 
@@ -48,10 +45,9 @@ mic_Contract_4 = Contract(
     contract_Type=planetary,
     contract_Size=extra_Small,
     from_Location=port_Tressler,
-    deliveries=[
-        (5, "Aluminum", new_Babbage),
-        (4, "Aluminum", sakura_Gold)
-    ],
+    deliveries=[(5, "Aluminum", new_Babbage),
+                (4, "Aluminum", sakura_Gold)
+                ],
     max_Container=1,
     contract_Pay=61500)
 
@@ -61,9 +57,8 @@ mic_Contract_5 = Contract(
     contract_Type=direct+planetary,
     contract_Size=small,
     from_Location=new_Babbage,
-    deliveries=[
-        (15, "Agricultural Supplies", port_Tressler)
-    ],
+    deliveries=[(15, "Agricultural Supplies", port_Tressler)
+                ],
     max_Container=4,
     contract_Pay=34500)
 
@@ -73,11 +68,109 @@ mic_Contract_6 = Contract(
     contract_Type=direct+planetary,
     contract_Size=extra_Small,
     from_Location=port_Tressler,
-    deliveries=[
-        (11, "Silicon", sakura_Gold)
-    ],
+    deliveries=[(11, "Silicon", sakura_Gold)
+                ],
     max_Container=1,
     contract_Pay=50250)
+
+mic_Contract_7 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=extra_Small,
+    from_Location=port_Tressler,
+    deliveries=[(11, "Titanium", sakura_Gold)
+                ],
+    max_Container=1,
+    contract_Pay=50250)
+
+mic_Contract_8 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=extra_Small,
+    from_Location=port_Tressler,
+    deliveries=[(10, "Carbon", sakura_Gold)
+                ],
+    max_Container=1,
+    contract_Pay=50250)
+
+mic_Contract_9 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=extra_Small,
+    from_Location=port_Tressler,
+    deliveries=[(10, "Quartz", sakura_Gold)
+                ],
+    max_Container=1,
+    contract_Pay=50250)
+
+mic_Contract_10 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=small,
+    from_Location=port_Tressler,
+    deliveries=[(10, "Agricultural Supplies", microTech_D13)
+                ],
+    max_Container=4,
+    contract_Pay=37250)
+
+mic_Contract_11 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=small,
+    from_Location=port_Tressler,
+    deliveries=[(18, "Hydrogen", microTech_D13)
+                ],
+    max_Container=4,
+    contract_Pay=43750)
+
+mic_Contract_12 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=small,
+    from_Location=microTech_D13,
+    deliveries=[(10, "Agricultural Supplies", port_Tressler),
+                ],
+    max_Container=4,
+    contract_Pay=45000)
+
+mic_Contract_13 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=small,
+    from_Location=port_Tressler,
+    deliveries=[(15, "Agricultural Supplies", new_Babbage),
+                ],
+    max_Container=4,
+    contract_Pay=37250)
+
+mic_Contract_14 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=extra_Small,
+    from_Location=port_Tressler,
+    deliveries=[(7, "Aluminum", new_Babbage),
+                ],
+    max_Container=1,
+    contract_Pay=50250)
+
+mic_Contract_15 = Contract(
+    contract_Origin=microTech,
+    contract_Rank=rookie,
+    contract_Type=direct+planetary,
+    contract_Size=small,
+    from_Location=port_Tressler,
+    deliveries=[(12, "Tin", sakura_Gold),
+                ],
+    max_Container=4,
+    contract_Pay=37250)
 
 # Automatically collect all mic_Contract_* variables into a list
 microTech_contracts = [
